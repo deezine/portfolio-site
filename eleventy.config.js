8952f51d09660c89fa759badd4c4c4bf813fa461
Frankie Roda <designwithfrankie@gmail.com>
2026-08-14 09:06:55 -0400

    readme.txt added
    Added a readme that I can update in the future.
    
    Also updating my eleventy.config and front matter as I teach myself how to use it properly.

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,55 +1,31 @@
-// Import html-minifier from html-minifier-terser
-import htmlmin from "html-minifier-terser";
+export default function (eleventyConfig) {
+  // --- Directories ---
+  // * Sets the input directory to /src/.
+  eleventyConfig.setInputDirectory("src");
+  // * Sets the output directory to /dist/.
+  eleventyConfig.setOutputDirectory("dist");
 
-// Import sass functionality from Sass.
-import sass from "sass";
-
-export default async function (eleventyConfig) {
-  // --- === | COPY | === ---
-  // * Move + Watch src/assets > dist/assets
-  eleventyConfig.addWatchTarget("src/assets");
+  // --- Assets ---
+  // * Moves assets from src > dist.
   eleventyConfig.addPassthroughCopy("src/assets");
+  // * Watches assets for changes.
+  eleventyConfig.addWatchTarget("src/assets");
 
-  // * Move + Watch src/robots.txt > dist/robots.txt
-  eleventyConfig.addWatchTarget("src/robots.txt");
-  eleventyConfig.addPassthroughCopy("src/robots.txt");
-
-  // * Move + Watch src/css > dist/css
-  eleventyConfig.addWatchTarget("src/css/*.css");
+  // --- CSS ---
+  // * Moves main.css from src > dist.
   eleventyConfig.addPassthroughCopy("src/css/*.css");
+  // * Watches main.css for changes.
+  eleventyConfig.addWatchTarget("src/css/*.css");
 
-  // --- === | MINIFY HTML | === ---
-  eleventyConfig.addTransform("htmlmin", function (content) {
-    // * If the page ends with .html...
-    if ((this.page.outputPath || "").endsWith(".html")) {
-      // * ...minify the HTML via the following settings:
-      let minified = htmlmin.minify(content, {
-        // * Use the shorthand for doctype.
-        useShortDoctype: true,
-        // * Remove all comments.
-        removeComments: true,
-        // * Collapse and condense all white space.
-        collapseWhitespace: true,
-      });
-
-      return minified;
-    }
-
-    // * If it is not .html, return the content as-is.
-    return content;
-  });
-
-  // --- === | CORE | === ---
-  return {
-    // * Uses NJK for HTML.
-    htmlTemplateEngine: "njk",
-
-    // * Configuration for directory
-    dir: {
-      input: "src",
-      output: "dist",
-      includes: "_includes",
-      layouts: "_layouts",
-    },
-  };
+  // --- Robots ---
+  // * Moves robots.txt from src > dist.
+  eleventyConfig.addPassthroughCopy("src/robots.txt");
+  // * Watches robots.txt for changes.
+  eleventyConfig.addWatchTarget("src/robots.txt");
 }
+
+export const config = {
+  // * Use Nunjuks for HTML and Markdown.
+  htmlTemplateEngine: "njk",
+  markdownTemplateEngine: "njk",
+};
---
+++ src/readme.md
@@ -0,0 +1,18 @@
+# Frankie's Portfolio Site
+
+## About
+
+This is the repository for my personal portfolio site. It's been quite the learning experience creating this from scratch, especially with regard to translating design to development.
+
+While I already knew basic HTML and CSS, throughout this project I not only gained a better understanding of both of those, but also taught myself NPM, SASS, and Eleventy.
+
+## What I Used
+
+VSCodium
+Stylelint
+Prettier
+NPM
+HTML
+CSS
+SASS
+Eleventy
